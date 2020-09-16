43134fbfa3c9a7d59741530524f1d534b754c5df
erikzaadi <erik.zaadi@gmail.com>
2020-09-16 10:34:02 +0300

    Add wowinterface

--- ci.sh
+++ ci.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh
+RELEASE=${1:-NOPE}
 chmod a+x ./release.sh
-./release.sh -g 1.13.5
+PARAMS="-g 1.13.5"
+if [[ "${RELEASE}" -eq "NOPE" ]]; then
+    PARAMS="${PARAMS} -d"
+fi
+curl -s https://raw.githubusercontent.com/BigWigsMods/packager/master/release.sh | bash -s -- ${PARAMS}
